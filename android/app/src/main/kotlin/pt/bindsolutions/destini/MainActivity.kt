fa9889af195e9e846edae6a32c69cc56be69b4a1
joaoantunesguimaraes <joao.antunes.guimaraes@gmail.com>
2022-10-13 18:07:27 +0100

    Commit Inicial

---
+++ android/app/src/main/kotlin/pt/bindsolutions/destini/MainActivity.kt
@@ -0,0 +1,6 @@
+package pt.bindsolutions.destini
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/story.dart
@@ -0,0 +1,13 @@
+class Story {
+  // Atributos
+  String storyTitle;
+  String choice1; // String ou int ?
+  String choice2; // String ou int ?
+
+  // Construtor - com named arguments - Argumentos Nomeados
+  //Story(this.storyTitle, this.choice1, this.choice2);
+  Story({required this.storyTitle, required this.choice1, required this.choice2});
+
+}
+
+
